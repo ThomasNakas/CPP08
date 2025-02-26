ef99a41d1c61cc4281409638dffb332a85c463fe
Thomas Nakas <tnakas@3-E-8.42heilbronn.de>
2025-02-26 01:04:04 +0100

    ex00 debugged and tested with vectors and lists

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -6,11 +6,35 @@
 /*   By: tnakas <tnakas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 19:52:06 by tnakas            #+#    #+#             */
-/*   Updated: 2025/02/25 19:56:44 by tnakas           ###   ########.fr       */
+/*   Updated: 2025/02/26 00:56:25 by tnakas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef  EASYFIND_HPP
 #define EASYFIND_HPP
 
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <list>
+#include <vector>
+#include <exception>
+
+class NotFoundException : public std::exception
+{
+	public:
+		const char* what() const throw()
+		{
+			return "Element not found in the container";
+		}
+};
+
+template <typename T>
+typename T::iterator easyfind(T& container, int value)
+{
+	typename T::iterator it =std::find(container.begin(), container.end(), value);
+	if (it == container.end())
+		throw NotFoundException();
+	return it;
+};
 #endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tnakas <tnakas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 12:05:19 by tnakas            #+#    #+#             */
-/*   Updated: 2025/02/25 20:00:02 by tnakas           ###   ########.fr       */
+/*   Updated: 2025/02/26 00:54:27 by tnakas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,21 @@
 
 int main()
 {
+	try
+	{
+		std::vector<int> vec = {1, 2, 3, 4, 5};
+		std::cout << "Found: " << *easyfind(vec, 3) << "\n";
+		
+		std::list<int> lst = {10, 20, 30, 40, 50};
+		std::cout << "Found: " << *easyfind(lst, 20) << "\n";
+
+		std::cout << "Seaching for 100... \n";
+		std::cout << "Found :" << *easyfind(vec, 100) << "\n";
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Exception: " << e.what() << "\n";
+	}
+	
 	return 0;
 }
