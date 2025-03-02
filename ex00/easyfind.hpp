704567660086b4cf70f683f274a285fbed133876
Thomas Nakas <tnakas@2-E-4.42heilbronn.de>
2025-03-02 17:06:29 +0100

    ex00: replacing the exception class with a logic error message for more siplicity

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: tnakas <tnakas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 19:52:06 by tnakas            #+#    #+#             */
-/*   Updated: 2025/03/02 16:56:29 by tnakas           ###   ########.fr       */
+/*   Updated: 2025/03/02 17:05:19 by tnakas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,23 +20,12 @@
 #include <vector>
 #include <exception>
 
-//notfoundexception class  inhereted from the exception class
-//I'm setting it in oreder to call it with a customizable message
-class NotFoundException : public std::exception
-{
-	public:
-		const char* what() const throw()
-		{
-			return "Element not found in the container";
-		}
-};
-
 template <typename T>
 typename T::iterator easyfind(T& container, int value)
 {
 	typename T::iterator it =std::find(container.begin(), container.end(), value);
 	if (it == container.end())
-		throw NotFoundException();
+		throw std::logic_error("Element not found in the container");
 	return it;
 };
 #endif
