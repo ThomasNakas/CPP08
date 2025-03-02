a957217b4ee4ee6dd553f11ab1f10e7bbfdac835
Thomas Nakas <tnakas@2-E-4.42heilbronn.de>
2025-03-02 17:04:08 +0100

    cleaning the ex00 test

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tnakas <tnakas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 12:05:19 by tnakas            #+#    #+#             */
-/*   Updated: 2025/03/02 16:48:07 by tnakas           ###   ########.fr       */
+/*   Updated: 2025/03/02 17:03:28 by tnakas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int main()
 		std::cout << "Found: " << *easyfind(lst, 20) << "\n";
 		std::cout << "//---exception test---//\n";
 		std::cout << "Seaching for 100... \n";
-		std::cout << "Found :" << *easyfind(vec, 100) << "\n";
+		easyfind(vec, 100);
 	}
 	catch(const std::exception& e)
 	{
