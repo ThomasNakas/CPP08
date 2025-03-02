92c397ec345a8a38fb28abafa194aa2486a2dd62
Thomas Nakas <tnakas@2-E-4.42heilbronn.de>
2025-03-02 15:12:23 +0100

    adding ex02 untested

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -6,11 +6,52 @@
 /*   By: tnakas <tnakas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 19:51:04 by tnakas            #+#    #+#             */
-/*   Updated: 2025/02/25 19:58:21 by tnakas           ###   ########.fr       */
+/*   Updated: 2025/03/02 14:57:07 by tnakas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MUNTANTSTACK_HPP
 #define MUNTANTSTACK_HPP
+
+#include <stack>
+#include <deque>
+#include <list>
+#include <iostream>
+
+template <typename T>
+class MuntantStack : public  std::stack<T>
+{
+	public:
+		//aliases
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+		typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+
+		MuntantStack() : std::stack<T>() {}
+		MuntantStack(const MuntantStack& other) : std::stack<T>(other) {}
+		MuntantStack& operator=(const MuntantStack& other)
+		{
+			if (this != &other)
+				std::stack<T>::operator=(other);
+			return *this;
+		}
+		~MuntantStack() {}
+		
+		//iterator functions
+		
+		iterator begin() {return this->c.begin();}
+		iterator end() {return this->c.end();}
+
+		const_iterator begin() const { return this->c.begin();}
+		const_iterator end() const { return this->c.end();}
+	
+		reverse_iterator rbegin() { return this->c.rbegin();}
+		reverse_iterator rend() {return this->c.rend();}
+		
+		const_reverse_iterator rbegin() const {return this->c.rbegin();}
+		const_reverse_iterator rend() const {return this-> c.rend();} 
+		
+};
 
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tnakas <tnakas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 12:05:19 by tnakas            #+#    #+#             */
-/*   Updated: 2025/02/25 20:01:07 by tnakas           ###   ########.fr       */
+/*   Updated: 2025/03/02 15:11:34 by tnakas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,51 @@
 
 int main()
 {
+	std::cout << "testing MuntantStack:\n";
+
+	MuntantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << "top element: " << mstack.top() << "\n";
+	mstack.pop();
+	std::cout << "Size after pop: " << mstack.size() << "\n";
+	
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(0);
+	
+	std::cout << "Stack elements: \n";
+	MuntantStack<int>::iterator it = mstack.begin();
+	MuntantStack<int>::iterator ite = mstack.end();
+	while (it != ite)
+	{
+		std::cout << *it << " ";
+		++it;
+	}
+	std::cout << "\n";
+
+	std::cout << "Stack elements (rbegin to rend): \n";
+	MuntantStack<int>::reverse_iterator rit = mstack.rbegin();
+	MuntantStack<int>::reverse_iterator rite = mstack.rend();
+	while (rit != rite)
+	{
+		std::cout << *rit << " ";
+		++rit;
+	}
+	std::cout << "\n";
+	
+	std:: cout << "Testing the copy: \n\n";
+	std::stack<int> s(mstack);
+	std::cout << "Top of copied std::stack " << s.top() << "\n";
+	
+	//comparing with std::list
+	std::cout << "Comparing with std::list:\n";
+	std::list<int> lstack;
+	lstack.push_back(3);
+	lstack.push_back(5);
+	lstack.push_back(737);
+	lstack.push_back(0);
+
 	return 0;
 }
